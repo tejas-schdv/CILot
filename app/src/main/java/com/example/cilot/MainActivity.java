65c747b3b39ba16b37db177bda33f7120830ef3c
ICHeadache <ICHeadache@hotmail.com>
2019-11-16 20:45:34 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/cilot/MainActivity.java
+++ app/src/main/java/com/example/cilot/MainActivity.java
@@ -226,8 +226,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 bottomSheetA11.show(getSupportFragmentManager(), "exampleBottomSheet");
                 break;
         }
-
-
     }
 
 
