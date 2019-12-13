7c91389fbeda3c09d91a9db024d104e510bbf4f5
Alsabery <56277621+Alsabery@users.noreply.github.com>
2019-12-12 20:11:22 -0800

    Current branch

--- app/src/main/java/com/example/cilot/profile_icons.java
+++ app/src/main/java/com/example/cilot/profile_icons.java
@@ -1,11 +1,14 @@
 package com.example.cilot;
 
 import android.content.Intent;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,6 +42,8 @@ public class profile_icons extends AppCompatActivity {
     Button Home;
     int playerLevel = 1;
     GoogleSignInClient mGoogleSignInClient;
+    Button lock1_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +62,7 @@ public class profile_icons extends AppCompatActivity {
         user_points = FirebaseDatabase.getInstance().getReference().child("users").child("107703088750367185275").child("points");
         user_level = FirebaseDatabase.getInstance().getReference().child("users").child("107703088750367185275").child("level");
         level.setText(String.valueOf(playerLevel));
+        lock1_button = findViewById(R.id.level1_button);
 
         user_points.addValueEventListener(new ValueEventListener() {
             @Override
@@ -69,12 +75,25 @@ public class profile_icons extends AppCompatActivity {
                 if (Integer.parseInt(dataSnapshot.getValue().toString()) < 100) {
                     playerLevel = 1;
                     level.setText(String.valueOf(1));
+                    ImageView image1 = (ImageView) findViewById(R.id.lock_lvl1);
+                    image1.setImageResource(R.drawable.echo);
                 } else if (Integer.parseInt(dataSnapshot.getValue().toString()) > 99 && Integer.parseInt(dataSnapshot.getValue().toString()) < 200) {
                     playerLevel = 2;
                     level.setText(String.valueOf(2));
+                    ImageView image1 = (ImageView) findViewById(R.id.lock_lvl1);
+                    image1.setImageResource(R.drawable.echo);
+                    ImageView image2 = (ImageView) findViewById(R.id.lock_lvl2);
+                    image2.setImageResource(R.drawable.buffy);
+
                 } else {
                     playerLevel = 3;
                     level.setText(String.valueOf(3));
+                    ImageView image1 = (ImageView) findViewById(R.id.lock_lvl1);
+                    image1.setImageResource(R.drawable.echo);
+                    ImageView image2 = (ImageView) findViewById(R.id.lock_lvl2);
+                    image2.setImageResource(R.drawable.buffy);
+                    ImageView image3 = (ImageView) findViewById(R.id.lock_lvl3);
+                    image3.setImageResource(R.drawable.ricardo);
                 }
             }
 
@@ -110,6 +129,7 @@ public class profile_icons extends AppCompatActivity {
             }
         });
 
+
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,6 +138,21 @@ public class profile_icons extends AppCompatActivity {
             }
         });
 
+        lock1_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(profile_icons.this, com.example.cilot.MainActivity.class)
+                //if (playerLevel > 0)
+                {
+                  //  final View image = new View()
+                }
+
+                //ImageView image = findViewById(R.id.nav_icon);
+                //image.setImageResource(R.drawable.ricardo);
+                //Intent intent = new Intent(profile_icons.this, com.example.cilot.MainActivity.class);
+                //startActivity(intent);
+            }
+        });
 
         final GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         final String uid = account.getId();
