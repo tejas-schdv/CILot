b16d7d9be5a9fe924bfdc79f6cd8e466f42707f6
Alsabery <56277621+Alsabery@users.noreply.github.com>
2019-12-12 19:06:37 -0800

    Created onclicklistener for lock button

--- app/src/main/java/com/example/cilot/profile_icons.java
+++ app/src/main/java/com/example/cilot/profile_icons.java
@@ -1,9 +1,11 @@
 package com.example.cilot;
 
 import android.content.Intent;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -40,6 +42,7 @@ public class profile_icons extends AppCompatActivity {
     Button Home;
     int playerLevel = 1;
     GoogleSignInClient mGoogleSignInClient;
+    Button lock1_button;
 
 
     @Override
@@ -59,6 +62,7 @@ public class profile_icons extends AppCompatActivity {
         user_points = FirebaseDatabase.getInstance().getReference().child("users").child("107703088750367185275").child("points");
         user_level = FirebaseDatabase.getInstance().getReference().child("users").child("107703088750367185275").child("level");
         level.setText(String.valueOf(playerLevel));
+        lock1_button = findViewById(R.id.level1_button);
 
         user_points.addValueEventListener(new ValueEventListener() {
             @Override
@@ -88,7 +92,7 @@ public class profile_icons extends AppCompatActivity {
                     image1.setImageResource(R.drawable.echo);
                     ImageView image2 = (ImageView) findViewById(R.id.lock_lvl2);
                     image2.setImageResource(R.drawable.buffy);
-                    ImageView image3 = (ImageView) findViewById(R.id.lock_lvl4);
+                    ImageView image3 = (ImageView) findViewById(R.id.lock_lvl3);
                     image3.setImageResource(R.drawable.ricardo);
                 }
             }
@@ -125,6 +129,7 @@ public class profile_icons extends AppCompatActivity {
             }
         });
 
+
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -133,6 +138,21 @@ public class profile_icons extends AppCompatActivity {
             }
         });
 
+        lock1_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(profile_icons.this, com.example.cilot.MainActivity.class)
+                //if (playerLevel > 0)
+                {
+                  //  final View image = new View()
+                }
+
+                //ImageView image = findViewById(R.id.nav_icon);
+                //image.setImageResource(R.drawable.ricardo);
+                //Intent intent = new Intent(profile_icons.this, com.example.cilot.MainActivity.class);
+                //startActivity(intent);
+            }
+        });
 
         final GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         final String uid = account.getId();
